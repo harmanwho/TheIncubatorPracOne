070897e7176dc22da4845ca67bc25ce4c31d7ab0
amdacccache <amdaccache@gmail.com>
2020-10-12 19:01:24 -0700

    Update DataDefScript.sql

--- DataDefScript.sql
+++ DataDefScript.sql
@@ -5,19 +5,31 @@
 -- --------------------------------------------------------
 
 --
--- Table structure for table `TableName`
+-- Table structure for table `People`
 --
-
-
---
--- Table structure for table `TableName`
---
-
+CREATE TABLE People(SchoolID INT PRIMARY KEY,
+		    fullname TEXT,
+		    isQuar BOOLEAN,
+                    groupID INT);
 
 --
--- Table structure for table `TableName`
+-- Table structure for table `Students`
 --
+CREATE TABLE Student(SchoolID INT,
+                     fullname TEXT,
+                     SpaceRoomID INT,
+                     classYear [9, 10, 11, 12],
+                     CONSTRAINT SchoolID_fk FOREIGN KEY (SchoolID) REFERENCES People(SchoolID),
+                     CONSTRAINT fullname_fk FOREIGN KEY (fullname) REFERENCES People(fullname),
+                     CONSTRAINT SpaceRoomID_fk FOREIGN KEY (SpaceRoomID) REFERENCES SpaceRoom(SpaceRoomID));
 
+--
+-- Table structure for table `Building`
+--
+CREATE TABLE Building(buildingID INT AUTO INCREMENT PRIMARY KEY,
+                      buildingName TEXT,
+                      buildingType [ACADEMIC, RESIDENTIAL,
+                                    ADMINISTRATIVE, EXTRACURRICULAR]);
 
 
 --
