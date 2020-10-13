c2a11e8aee44ecebf8e091014a2d018bff60cc99
Harman Sidhu <harmandeepsinghful@gmail.com>
2020-10-12 18:58:58 -0700

    Basic .sql file structure included

---
+++ DataDefScript.sql
@@ -0,0 +1,32 @@
+--
+-- Database: `dbName`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `TableName`
+--
+
+
+--
+-- Table structure for table `TableName`
+--
+
+
+--
+-- Table structure for table `TableName`
+--
+
+
+
+--
+-- Table structure for table `TableName`
+--
+
+
+--
+-- Reference: if any
+-- Note: if any
+--
+
