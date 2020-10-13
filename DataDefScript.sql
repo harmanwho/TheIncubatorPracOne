ebb9c39f62316d8f6253dfb2b3efc52de5850ef8
Harman Sidhu <harmandeepsinghful@gmail.com>
2020-10-12 19:40:07 -0700

    Syntax Changes | Added AUTO_INCREMENT

--- DataDefScript.sql
+++ DataDefScript.sql
@@ -7,58 +7,75 @@
 --
 -- Table structure for table `People`
 --
-CREATE TABLE People(SchoolID INT PRIMARY KEY,
-		    fullname TEXT,
-		    isQuar BOOLEAN,
-                    groupID INT);
+CREATE TABLE People (
+    SchoolID INT PRIMARY KEY,
+	FullName TEXT NOT NULL,
+	IsQuar BOOLEAN,
+    GroupID INT NOT NULL
+);
 
 --
 -- Table structure for table `Students`
 --
-CREATE TABLE Student(SchoolID INT,
-                     fullname TEXT,
-                     SpaceRoomID INT,
-                     classYear [9, 10, 11, 12],
-                     CONSTRAINT SchoolID_fk FOREIGN KEY (SchoolID) REFERENCES People(SchoolID),
-                     CONSTRAINT fullname_fk FOREIGN KEY (fullname) REFERENCES People(fullname),
-                     CONSTRAINT SpaceRoomID_fk FOREIGN KEY (SpaceRoomID) REFERENCES SpaceRoom(SpaceRoomID));
+CREATE TABLE Student (
+    SchoolID INT NOT NULL,
+    FullName TEXT NOT NULL,
+    SpaceRoomID INT NOT NULL,
+    ClassYear(col SET(9, 10, 11, 12)),
+    CONSTRAINT SchoolID_fk FOREIGN KEY (SchoolID) REFERENCES People(SchoolID),
+    CONSTRAINT Fullname_fk FOREIGN KEY (Fullname) REFERENCES People(Fullname),
+    CONSTRAINT SpaceRoomID_fk FOREIGN KEY (SpaceRoomID) REFERENCES SpaceRoom(SpaceRoomID)
+);
 
 --
 -- Table structure for table `Building`
 --
-CREATE TABLE Building(buildingID INT AUTO INCREMENT PRIMARY KEY,
-                      buildingName TEXT,
-                      buildingType [ACADEMIC, RESIDENTIAL,
-                                    ADMINISTRATIVE, EXTRACURRICULAR]);
+CREATE TABLE Building (
+    BuildingID INT PRIMARY KEY AUTO_INCREMENT,
+    BuildingName TEXT,
+    BuildingType [ACADEMIC, RESIDENTIAL, ADMINISTRATIVE, EXTRACURRICULAR]
+);
 
 
 --
 -- Table structure for table `SpaceRoom`
 --
-CREATE TABLE SpaceRoom(SpaceRoomID INT AUTO INCREMENT PRIMARY KEY,
-                       SpaceName TEXT,
-                       buildingID INT,
-                       CONSTRAINT buildingID_fk FOREIGN KEY (buildingID) REFERENCES Building(buildingID));
+CREATE TABLE SpaceRoom (
+    SpaceRoomID INT NOT NULL AUTO_INCREMENT,
+    SpaceName TEXT,
+    BuildingID INT,
+    CONSTRAINT BuildingID_fk FOREIGN KEY (BuildingID) REFERENCES Building(BuildingID)
+);
+
+
 --
 -- Table structure for table `Event`
 --
-CREATE TABLE Event(EventID INT AUTO INCREMENT PRIMARY KEY,
-                   SpaceRoomID INT,
-                   startTime TIME,
-                   endTime TIME,
-                   EventName TEXT,
-                   day DATE,
-                   CONSTRAINT SpaceRoomID_fk FOREIGN KEY (SpaceRoomID) REFERENCES SpaceRoom(SpaceRoomID));
+CREATE TABLE Event (
+    EventID INT PRIMARY KEY AUTO_INCREMENT,
+    SpaceRoomID INT,
+    StartTime TIME,
+    EndTime TIME,
+    EventName TEXT,
+    EventDay DATE,
+    CONSTRAINT SpaceRoomID_fk FOREIGN KEY (SpaceRoomID) REFERENCES SpaceRoom(SpaceRoomID)
+);
+
+
 --
 -- Table structure for table `EventAttendees`
 --
-CREATE TABLE EventAtendees(SchoolID INT,
-                           AtendeeID INT AUTO INCREMENT PRIMARY KEY,
-                           EventID INT,
-                           CONSTRAINT SchoolID_fk FOREIGN KEY (SchoolID) REFERENCES Person(SchoolID),
-                           CONSTRAINT EventID_fk FOREIGN KEY (EventID) REFERENCES Event(EventID));			    
+CREATE TABLE EventAtendees (
+    AttendeeID INT PRIMARY KEY AUTO_INCREMENT,
+    SchoolID INT,
+    EventID INT,
+    CONSTRAINT SchoolID_fk FOREIGN KEY (SchoolID) REFERENCES Person(SchoolID),
+    CONSTRAINT EventID_fk FOREIGN KEY (EventID) REFERENCES Event(EventID)
+);			
+
+
 --
--- Reference: if any
+-- References: if any
 -- Note: if any
 --
 
